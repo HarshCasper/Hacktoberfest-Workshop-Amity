eb239a43b58efe3fd925163497fc46d7f92b84d4
hrudaikoda <hrudai.mastermind@gmail.com>
2020-10-01 20:41:14 +0530

    Updated js file

--- script/data.js
+++ script/data.js
@@ -28,5 +28,10 @@ const data = `[{
 		"name": "Debasis Jana",
 		"desc": "DevOps engineer",
 		"github": "https://github.com/deba19"
+	},
+	{
+		"name": "Hrudai Koda",
+		"desc": "Blender enthusiast",
+		"github": "https://github.com/hrudaikoda"
 	}
 ]`;
