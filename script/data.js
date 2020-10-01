a1a40b737a9ff04c9a0f0ec1a99d209c27d915e1
Abita Ann Augustine <abitaaugustine@gmail.com>
2020-10-01 20:59:32 +0530

    Added my Name to the site

--- script/data.js
+++ script/data.js
@@ -28,7 +28,7 @@ const data = `[{
 		"name": "Debasis Jana",
 		"desc": "DevOps engineer",
 		"github": "https://github.com/deba19"
-	}
+	},
 	{
   		"name": "Abita Ann Augustine",
   		"desc": "Software Developer and Microsoft Azure Machine Learning Scholarship Winner",
