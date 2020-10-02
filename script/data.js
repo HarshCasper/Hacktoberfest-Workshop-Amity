d801a3205ee46b46101c1e9fcfe819298d919cf3
Alexy Pulivelil <44542655+AlexyPulivelil@users.noreply.github.com>
2020-10-02 16:48:18 +0530

    Update data.js

--- script/data.js
+++ script/data.js
@@ -33,7 +33,7 @@ const data = `[{
   		"name": "Abita Ann Augustine",
   		"desc": "Software Developer and Microsoft Azure Machine Learning Scholarship Winner",
  		"github": "https://github.com/abitaaugustine"
- 	}
+ 	},
 	{
 		"name": "Alexy P Thomas",
 		"desc": Front-end Developer and newbie in Flutter Development"
