e6dd30c3cf8455c2076d391c479881db11c435d6
Harshith B <64398730+HarshithB@users.noreply.github.com>
2020-10-06 17:54:25 +0530

    Update data.js

--- script/data.js
+++ script/data.js
@@ -30,6 +30,11 @@ const data = `[{
 		"github": "https://github.com/deba19"
 	},
 	{
+		"name": "Harshith B",
+		"desc": "Software Developer",
+		"github": "https://github.com/HarshithB"
+	},
+	{
   		"name": "Abita Ann Augustine",
   		"desc": "Software Developer and Microsoft Azure Machine Learning Scholarship Winner",
  		"github": "https://github.com/abitaaugustine"
