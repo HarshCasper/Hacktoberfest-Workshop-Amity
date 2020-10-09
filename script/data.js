c7410406cf1ce345449dcea6545d0e05fb413527
Keshav Agrawal <56186828+Keshav04042001@users.noreply.github.com>
2020-10-09 23:03:42 +0530

    made changes according to issue

--- script/data.js
+++ script/data.js
@@ -33,7 +33,7 @@ const data = `[{
   		"name": "Abita Ann Augustine",
   		"desc": "Software Developer and Microsoft Azure Machine Learning Scholarship Winner",
  		"github": "https://github.com/abitaaugustine"
- 	}
+ 	},
 	{
 		"name": "Keshav Agrawal",
 		"desc": "Full stack web developer",
